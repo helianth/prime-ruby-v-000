0867fb3c3b2f6eeb91caefa20b614cb97a9f2037
Lamia Haider <lamia.haider@gmail.com>
2018-04-22 03:45:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,17 @@
 # Add  code here!
 def prime?(n)
-  if n < 0
-    n * -1
-  end
-  if n != 2 and n%2 == 0 
+  if n != 2 and n%2 == 0
     return false
   end
+  if n < 0
+    n * -1
+    list = (2..to_n).to_a
+    list.each do |i|
+      if n%i == 0
+        return false
+      else return true
+      end
+    end
+  end
+end
   
