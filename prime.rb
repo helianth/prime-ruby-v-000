b470afaab7d86beb827b2d2eb0bc01e1273eab08
Lamia Haider <lamia.haider@gmail.com>
2018-04-22 03:47:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,14 +3,11 @@ def prime?(n)
   if n != 2 and n%2 == 0
     return false
   end
-  if n < 0
-    n * -1
-    list = (2..n).to_a
-    list.each do |i|
-      if n%i == 0
-        return false
-      else return true
-      end
+  list = (2..n).to_a
+  list.each do |i|
+    if n%i == 0
+      return false
+    else return true
     end
   end
 end
